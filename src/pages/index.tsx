4082b5ff31020afb231b32b2fd314bde12d14760
Kasra <kasra.koushan@gmail.com>
2024-02-24 18:44:37 -0500

    fix error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { PlainEditor } from "@/components/PlainEditor";
-import { SlateEditor } from "@/components/SlateEditor";
 
 const inter = Inter({ subsets: ["latin"] });
 
