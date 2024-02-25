62e0e560a9ce541375f069bab29a9bbec12605ea
Kasra <kasra.koushan@gmail.com>
2024-02-25 00:48:53 -0500

    trying to get the animation to land at theright place (position is off slightly)

--- src/components/PlainEditor.tsx
+++ src/components/PlainEditor.tsx
@@ -1,9 +1,50 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from './PlainEditor.module.css';
 
 interface CharAnimation {
   id: number;
   char: string;
+  animationStyle: string;
+}
+
+// extremely hacky
+function addStyleElement(content: string) {
+  const styleEl = document.createElement("style");
+  document.head.appendChild(styleEl);
+  styleEl.sheet?.insertRule(content, 0);
+}
+
+function shrinkToPositionKeyframe({ top, left, name }: { top: number, left: number, name: string }) {
+  return `
+  @keyframes ${name} {
+    from {
+        font-size: 100vw;
+    }
+    to {
+        font-size: 0;
+        top: ${top}px;
+        left: ${left}px;
+    }
+  }
+  `;
+}
+
+const FONT_SIZE = 14;
+
+const RATIO = 0.8;
+
+function calculatePosition(
+  { textAreaTop, textAreaLeft, textAreaWidth }: { textAreaTop: number, textAreaLeft: number, textAreaWidth: number },
+  currentText: string): {
+    top: number, left: number
+  } {
+  console.log(`top: ${textAreaTop}, left: ${textAreaLeft}, width: ${textAreaWidth}`);
+  const textWidth = currentText.length * FONT_SIZE;
+  const numLines = Math.floor(textWidth / textAreaWidth);
+  const top = textAreaTop + numLines * FONT_SIZE;
+  const left = textAreaLeft + (textWidth % textAreaWidth);
+
+  return { top, left };
 }
 
 export function PlainEditor() {
@@ -12,6 +53,12 @@ export function PlainEditor() {
   const [nextId, setNextId] = useState(0);
   const [spin, setSpin] = useState(false);
   const [curl, setCurl] = useState(false);
+  const textAreaRef = useRef(null);
+
+  // text area
+  const [textAreaTop, setTextAreaTop] = useState(0);
+  const [textAreaLeft, setTextAreaLeft] = useState(0);
+  const [textAreaWidth, setTextAreaWidth] = useState(0);
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const newValue = event.target.value;
@@ -19,10 +66,17 @@ export function PlainEditor() {
 
     if (newValue.length > text.length) {
       const newChar = newValue.slice(-1);
+      const animationName = `animation-${nextId}`;
+      const animationStyle = `${animationName} 1s ease forwards`;
+
+      const { top, left } = calculatePosition({ textAreaLeft, textAreaTop, textAreaWidth }, text);
+
+      // add to stylesheet
+      addStyleElement(shrinkToPositionKeyframe({ top, left, name: animationName }));
 
       setAnimationQueue((prevQueue) => [
         ...prevQueue,
-        { id: nextId, char: newChar }
+        { id: nextId, char: newChar, animationStyle }
       ]);
       setNextId(id => id + 1);
     } else {
@@ -34,14 +88,24 @@ export function PlainEditor() {
       const timer = setTimeout(() => {
         // Remove the first character from the queue after its animation completes
         setAnimationQueue((prevQueue) => prevQueue.slice(1));
-      }, 2000); // Duration of the animation
+      }, 1000); // Duration of the animation
 
       return () => clearTimeout(timer);
     }
   }, [animationQueue]);
 
+  useEffect(() => {
+    if (textAreaRef.current) {
+      const { x, y, width } = (textAreaRef.current as any).getBoundingClientRect();
+      setTextAreaTop(y);
+      setTextAreaLeft(x);
+      setTextAreaWidth(width);
+    }
+  })
+
   return <div className={styles.container}>
     <h1 className={styles.header}>crusty the trusty text editor</h1>
+    <p>(checkboxes don't do anything atm)</p>
     <label className={styles.checkbox}>
       <input type="checkbox" checked={spin} onChange={() => setSpin(!spin)} />
       spin
@@ -50,10 +114,12 @@ export function PlainEditor() {
       <input type="checkbox" checked={curl} onChange={() => setCurl(!curl)} />
       curl
     </label>
-    <textarea className={styles.textArea} onInput={handleChange} />
+    <textarea ref={textAreaRef} className={styles.textArea} onInput={handleChange} />
     {animationQueue.map((item) => (
-      <div key={item.id} className={spin ? styles.shrinkAndSpinChar : (curl ? styles.curlyChar : styles.shrinkingChar)}>
-        {item.char}
+      <div key={item.id} className={styles.charContainer}>
+        <span className={styles.shrinkingChar} style={{ animation: item.animationStyle }}>
+          {item.char}
+        </span>
       </div>
     ))
     }
