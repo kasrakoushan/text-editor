ee67a58181f740e021e30b98c8be6bd6daa17403
Kasra <kasra.koushan@gmail.com>
2024-04-05 15:19:00 -0400

    remove opacity

--- src/app/util/animation.ts
+++ src/app/util/animation.ts
@@ -13,14 +13,14 @@ export function shrinkToPositionKeyframe({ top, left, name }: { top: number, lef
       left: 50%;
       font-size: 100vw;
       transform: translate(-50%, -50%);
-      opacity: 0.6;
+      // opacity: 0.6;
     }
     100% {
       top: ${top}px;
       left: ${left}px;
       font-size: 0;
       transform: translate(0, 0);
-      opacity: 1;
+      // opacity: 1;
     }
   }
   `;
